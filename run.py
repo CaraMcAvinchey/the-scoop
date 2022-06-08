77999df03dbe6eab5cbec6562eba7deecd2c9dc7
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2022-06-08 07:55:00 +0000

    Add validation for user name input

--- run.py
+++ run.py
@@ -25,25 +25,49 @@ def welcome_message():
     print("Welcome to The Scoop!")
     print("Enjoy a scoop (or two!) of ice cream with us.")
 
-# Customer Login/Registration
+# Customer Information
 
-def login_or_register():
+def user_info():
     """
-    Check if the user wants to log in or register.
-    Return the answer.
+    Collect the customer's first name for their order.
     """
-    print("Let's get scooping:\n")
-    print("Please select the following options. I'd like to...")
-    print("1 - LOGIN")
-    print("2 - REGISTER")
-    answer = input("Enter your answer here:\n").strip()
+    print("Let's get scooping!\n")
+    print("Tell us your name so we can call you when your order is ready.\n")
+    while True:
+        name = input("Enter your name here:\n").capitalize().strip()
 
-    end_section()
-    # Validate users input as correct
-    while answer not in ("1", "2"):
-        print("Please choose either option 1 or 2.")
-        answer = input("Enter your answer here:\n").strip()
-    return answer
+        end_section()
+
+        if validate_name(name):
+            print(f"Hello {name}")
+            break
+
+    return name
+
+def validate_name(name):
+    """
+    Validate customer name.
+    Check if the customer input numbers or characters.
+    Check if their name is at least 2 characters long.
+    """
+    try:
+        if name.isnumeric():
+            raise ValueError(
+                "Please input your name using letters only, not numbers.") 
+
+        if not name.isalpha():
+            raise ValueError(
+                "Please input your name using letters only, not characters.")          
+
+        if len(name) < 2:
+            raise ValueError(
+                "Your name needs to be at least two characters long.")
+
+    except ValueError as e:
+        print(f"{e} \nPlease try again.")
+        return False
+
+    return True   
 
 # Program Formatting
 def end_section():
@@ -54,7 +78,7 @@ def end_section():
     print("# "* 25)
     print(" ")
 
-# Main Function
+# Main Functions
 
 def main():
     """
@@ -63,8 +87,7 @@ def main():
     # Welcome message
     welcome_message()
     end_section()
-
-    #Login/Registration 
-    login_or_register()
+    #Customer Information
+    user_info()
 
 main()
